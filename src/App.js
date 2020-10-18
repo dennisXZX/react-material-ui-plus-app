ed2b0ff6c6293dbcd592bdca69477699f6bea7a3
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-10-18 18:21:43 +1000

    add theming demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, ThemeWrapper } from 'material-ui-plus';
 import './App.css';
 
+const DEFAULT_THEME = 'default';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, setTheme] = useState(DEFAULT_THEME);
+
+    return (
+        <ThemeWrapper theme={theme}>
+            <div className="App">
+                <h1>Theming Demo</h1>
+                <h2>You are currently on {theme} theme</h2>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setTheme('default')}
+                >
+                    Default Theme
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setTheme('happy')}
+                >
+                    Happy Theme
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setTheme('sad')}
+                >
+                    Sad Theme
+                </Button>
+            </div>
+        </ThemeWrapper>
+    );
 }
 
 export default App;
